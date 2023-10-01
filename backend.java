b7943774008c8b261a45e221801a6e8a4bfef896
KyleWynne <69371846+KyleWynne@users.noreply.github.com>
2023-10-01 13:52:26 -0700

    put block code skeletons and formatted the html+css

--- backend.java
+++ backend.java
@@ -3,6 +3,27 @@ var workspace = Blockly.inject('blocklyDiv', {
     toolbox: document.getElementById('toolbox') // Optional toolbox
 });
 
+Blockly.JavaScript.forBlock['Exercise'] = function(block) {
+  var var_name = Blockly.JavaScript.valueToCode(block, 'VAR', Blockly.JavaScript.ORDER_NONE);
+  var expr_code = Blockly.JavaScript.valueToCode(block, 'EXPR', Blockly.JavaScript.ORDER_NONE);
+  var code = '(' + var_name + ' => ' + expr_code + ')';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+Blockly.JavaScript.forBlock['Diet'] = function(block) {
+  var expr1_code = Blockly.JavaScript.valueToCode(block, 'EXPR1', Blockly.JavaScript.ORDER_NONE);
+  var expr2_code = Blockly.JavaScript.valueToCode(block, 'EXPR2', Blockly.JavaScript.ORDER_NONE);
+  var code = '(' + expr1_code + ')(' + expr2_code + ')';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+Blockly.JavaScript.forBlock['Text'] = function(block) {
+  var expr1_code = Blockly.JavaScript.valueToCode(block, 'EXPR1', Blockly.JavaScript.ORDER_NONE);
+  var expr2_code = Blockly.JavaScript.valueToCode(block, 'EXPR2', Blockly.JavaScript.ORDER_NONE);
+  var code = '(' + expr1_code + ')(' + expr2_code + ')';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
 var runButton = document.getElementById('runButton');
 runButton.addEventListener('click', function() {
     var code = Blockly.JavaScript.workspaceToCode(workspace);
