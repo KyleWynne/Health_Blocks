ec670cd55b4efab253730221e8781913ebd5517d
Natalie Huante <huante@chapman.edu>
2023-10-01 15:27:45 -0700

    added js file and block definitions

---
+++ backend.js
@@ -0,0 +1,100 @@
+
+var workspace = Blockly.inject('blocklyDiv', {
+    toolbox: document.getElementById('toolbox') // Optional toolbox
+});
+
+
+Blockly.Blocks['exercise'] = {
+  init: function() {
+    this.appendValueInput("VAR")
+        .setCheck("var")
+        .appendField("exercise");
+    this.appendValueInput("EXPR")
+        .setCheck(["var", "exp", "Number"]);
+    this.setInputsInline(true);
+    this.setOutput(true, "exp");
+    this.setColour(130);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['diet'] = {
+  init: function() {
+    this.appendValueInput("EXPR1")
+        .setCheck(["var", "exp", "Number"])
+        .appendField("diet");
+    this.appendValueInput("EXPR2")
+        .setCheck(["var", "exp", "Number"]);
+    this.setInputsInline(true);
+    this.setOutput(true, "exp");
+    this.setColour(330);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+
+Blockly.Blocks['text'] = {
+  init: function() {
+    this.appendValueInput("EXPR1")
+        .setCheck(["var", "exp", "Number"])
+        .appendField("text");
+    this.appendValueInput("EXPR2")
+        .setCheck(["var", "exp", "Number"]);
+    this.setInputsInline(true);
+    this.setOutput(true, "exp");
+    this.setColour(330);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['math_number'] = {
+  /**
+   * Block for numeric value.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    this.setHelpUrl(Blockly.Msg.MATH_NUMBER_HELPURL);
+    this.setColour(Blockly.Msg.MATH_HUE);
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldNumber('3'), 'NUM');
+    this.setOutput(true, 'Number');
+    this.setTooltip(Blockly.Msg.MATH_NUMBER_TOOLTIP);
+  }
+};
+
+
+
+Blockly.JavaScript.forBlock['Exercise'] = function(block) {
+  var var_name = Blockly.JavaScript.valueToCode(block, 'VAR', Blockly.JavaScript.ORDER_NONE);
+  var expr_code = Blockly.JavaScript.valueToCode(block, 'EXPR', Blockly.JavaScript.ORDER_NONE);
+  var code = '(' + var_name + ' => ' + expr_code + ')';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+Blockly.JavaScript.forBlock['Diet'] = function(block) {
+  var expr1_code = Blockly.JavaScript.valueToCode(block, 'EXPR1', Blockly.JavaScript.ORDER_NONE);
+  var expr2_code = Blockly.JavaScript.valueToCode(block, 'EXPR2', Blockly.JavaScript.ORDER_NONE);
+  var code = '(' + expr1_code + ')(' + expr2_code + ')';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+Blockly.JavaScript.forBlock['Text'] = function(block) {
+  var expr1_code = Blockly.JavaScript.valueToCode(block, 'EXPR1', Blockly.JavaScript.ORDER_NONE);
+  var expr2_code = Blockly.JavaScript.valueToCode(block, 'EXPR2', Blockly.JavaScript.ORDER_NONE);
+  var code = '(' + expr1_code + ')(' + expr2_code + ')';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+var runButton = document.getElementById('runButton');
+runButton.addEventListener('click', function() {
+    var code = Blockly.JavaScript.workspaceToCode(workspace);
+    // Execute the generated JavaScript code
+    try {
+        eval(code);
+    } catch (error) {
+        console.error(error);
+    }
+});
