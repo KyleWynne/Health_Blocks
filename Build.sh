ea0dc177579fe9e307cddeb649ced49973a742ef
KyleWynne <69371846+KyleWynne@users.noreply.github.com>
2023-12-07 08:24:43 -0800

    Add files via upload

---
+++ Build.sh
@@ -0,0 +1,11 @@
+cd src
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install node
+node -v
+npm -v
+npm install fetch
+npm install cors
+npm install express
+npm install vm
+node server.js
+```
